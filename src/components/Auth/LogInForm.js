acf69255931865df5dba5a229569d3b22ac18c20
90sPixelDev <fontlien@gmail.com>
2022-06-11 09:25:01 -0400

    changed to firebase authentication for simpler data flow able to log in and sign up and the data belongsd to each unique user

---
+++ src/components/Auth/LogInForm.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const LogInForm = (props) => {
+	const classes = {
+		form: 'flex flex-col gap-4',
+		formTitle: 'mx-auto',
+		input: 'rounded-lg p-1',
+		signUpBtn:
+			'bg-green-500 px-2 py-1 rounded-lg border-[1px] border-green-600 text-white sm:hover:bg-green-400 transition',
+		logInBtn:
+			'bg-gray-700 px-2 py-1 text-white rounded-lg border-[1px] border-gray-800 sm:hover:bg-gray-600 transition',
+		or: 'mx-auto',
+	};
+
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const changeEmail = (e) => {
+		setEmail(e.target.value);
+	};
+	const changePassword = (e) => {
+		setPassword(e.target.value);
+	};
+
+	const userSignedIn = (e) => {
+		e.preventDefault();
+		const userInfo = { email, password };
+		setEmail('');
+		setPassword('');
+		props.onUserSignedIn(userInfo);
+	};
+
+	return (
+		<form className={classes.form}>
+			<h2 className={classes.formTitle}>Log In</h2>
+			<input
+				className={classes.input}
+				type='email'
+				name='email-input'
+				onChange={changeEmail}
+				placeholder='Email'
+				required='required'
+			></input>
+			<input
+				className={classes.input}
+				type='password'
+				name='password-input'
+				onChange={changePassword}
+				placeholder='Password'
+				required='required'
+			></input>
+			<button
+				className={classes.logInBtn}
+				type='submit'
+				onClick={userSignedIn}
+			>
+				Log In
+			</button>
+			<p className={classes.or}>Or</p>
+			<button
+				className={classes.signUpBtn}
+				type='button'
+				onClick={props.onSwitchForm}
+			>
+				Sign Up
+			</button>
+		</form>
+	);
+};
+
+LogInForm.propTypes = {
+	onSwitchForm: PropTypes.func,
+	onUserSignedIn: PropTypes.func,
+};
+
+export default LogInForm;
